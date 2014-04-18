032f2ad11369f9e3f00e30a525a7f037b0d4a487
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2014-04-18 23:47:57 +0100

    Minor tweaks to indentation.

--- src/main/scala/ScalazStream.scala
+++ src/main/scala/ScalazStream.scala
@@ -8,16 +8,13 @@ object ScalazStream extends App {
   val fromCallback: Process[Task, Throwable \/ String] = Process.repeatEval(
     // We need to fork because Task.async is not stack-safe
     //
-    Task.fork(
-      Task.async[String] { cb ⇒
-        if (math.random > 0.10) {
-          cb(\/-("No errors"))
-        } else {
-          cb(-\/(new Throwable("Some errors")))
-        }
-      } attempt
-    )
-  )
+    Task.fork(Task.async[String] { cb ⇒
+      if (math.random > 0.10) {
+        cb(\/-("No errors"))
+      } else {
+        cb(-\/(new Throwable("Some errors")))
+      }
+    } attempt))
   val printlnExc: Sink[Task, Throwable] = stdOutLines.contramap(_.getMessage)
   val printlnStr: Sink[Task, String] = stdOutLines
   val obtainFromQueueAndLog: Process[Task, Unit] = fromCallback.drainW(printlnExc).to(printlnStr)
